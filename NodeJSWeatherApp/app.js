c2976092bbc2f3d4c79c6eaada191748ea5f2ae4
tharri6 <timothyharrris@34.252.21.198.tigernet.wifi.dyn.clemson.edu>
2017-02-23 16:38:27 -0500

    added files
    blah

---
+++ NodeJSWeatherApp/app.js
@@ -0,0 +1,49 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+  request = require('request'),
+
+_ = require('lodash');
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      latitude:req.query.latitude,
+      longitude:req.query.longitude
+     };
+
+      var callURL = "https://27b63554-1d16-4e55-b39b-fcf26579c187:cTvShACMby@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US"
+
+      request.get(callURL, {
+        json: true
+      },
+      function (error, response, body) {
+       console.log(body)
+      });
+})
+
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
