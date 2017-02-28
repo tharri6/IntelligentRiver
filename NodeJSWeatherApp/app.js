68b50cd9be199ee8842b73d45a08216239c839fd
tharri6 <timothyharrris@11.254.21.198.tigernet.wifi.dyn.clemson.edu>
2017-02-28 16:10:53 -0500

    blah

--- NodeJSWeatherApp/app.js
+++ NodeJSWeatherApp/app.js
@@ -28,13 +28,11 @@ app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
    response = {
       latitude:req.query.latitude,
-      longitude:req.query.longitude
+      longitude:req.query.longitude,
+      start:req.query.start
      };
 
-      var callURL = "https://ae77e367-1cb5-4692-b518-
-     					ccf49ed673f1:1PM28RVfi5@twcservice.mybluemix.net/api/weather/v1/
-     					geocode/"+response.latitutde+"/"+response.longitude+"/almanac/daily.json?
-     					start="+response.start+"/units=m&language=en-US";
+      var callURL = "https://ae77e367-1cb5-4692-b518-ccf49ed673f1:1PM28RVfi5@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitutde+"/"+response.longitude+"/almanac/daily.json?start="+response.start+"/units=m&language=en-US";
 
       request.get(callURL, {
         json: true
